f8aa86bb1e53a841801b9d6437b273639d18a8c4
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-24 15:44:22 -0500

    Add clickOutside function to handle outside click events

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,23 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export function clickOutside(node: {
+	contains: (arg0: any) => any;
+	dispatchEvent: (arg0: CustomEvent<unknown>) => void;
+}) {
+	// the node has been mounted in the DOM
+
+	window.addEventListener('click', handleClick);
+
+	function handleClick(e: { target: any }) {
+		if (!node.contains(e.target)) {
+			node.dispatchEvent(new CustomEvent('outsideclick'));
+		}
+	}
+
+	return {
+		destroy() {
+			// the node has been removed from the DOM
+			window.removeEventListener('click', handleClick);
+		}
+	};
+}
