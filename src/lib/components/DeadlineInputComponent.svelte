2d4345064d28c515f3c95a5d36b5e5fc93a4d908
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-24 23:15:18 -0500

    Add deadline input component for task management

---
+++ src/lib/components/DeadlineInputComponent.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	import { db, type Task } from '$lib/db';
+	import { FlagSolid } from 'flowbite-svelte-icons';
+
+	let {
+		openedTask = $bindable(),
+		editingDeadlineForTaskId = $bindable()
+	}: {
+		openedTask: Task | null;
+		editingDeadlineForTaskId: number | null;
+	} = $props();
+
+	function toggleDeadlinePickerForTask(taskId: number) {
+		if (editingDeadlineForTaskId === taskId) {
+			editingDeadlineForTaskId = null;
+			return;
+		}
+
+		editingDeadlineForTaskId = taskId;
+	}
+
+	function setDeadlineForTask(event: Event) {
+		const dateValue = (event.target as HTMLInputElement).value;
+
+		// Set to UTC timezone
+		const selectedDate = dateValue ? new Date(dateValue + 'T23:59:59Z') : null;
+
+		if (openedTask) {
+			// Make sure the deadline is saved with time set to 00:00:00
+			if (selectedDate) {
+				selectedDate.setHours(0, 0, 0, 0);
+			}
+
+			db.tasks.update(openedTask.id, {
+				deadline: selectedDate,
+				updated_at: new Date()
+			});
+
+			openedTask.deadline = selectedDate;
+		}
+		editingDeadlineForTaskId = null;
+	}
+
+	function getDeadlineRelativeText(deadline: Date | null): string {
+		if (!deadline) return '';
+
+		const now = new Date();
+		const timeDiff = deadline.getTime() - now.getTime();
+		const dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+		if (dayDiff === 0) {
+			return 'today';
+		} else if (dayDiff >= 1) {
+			return `${dayDiff} days left`;
+		} else if (dayDiff === -1) {
+			return '1 day ago';
+		} else {
+			return `${-dayDiff} days ago`;
+		}
+	}
+</script>
+
+<button
+	class="flex cursor-pointer items-center rounded px-3 py-2 hover:text-gray-600"
+	onclick={() => openedTask && toggleDeadlinePickerForTask(openedTask.id)}
+>
+	<FlagSolid class="h-6 w-6 shrink-0" />
+</button>
+
+{#if openedTask && openedTask.deadline}
+	<div class="content-center text-sm text-gray-500">
+		{getDeadlineRelativeText(openedTask.deadline)}
+	</div>
+{/if}
+
+{#if openedTask && editingDeadlineForTaskId === openedTask.id}
+	<input
+		type="date"
+		class="ml-4 rounded border border-gray-300 p-2"
+		value={openedTask.deadline?.toISOString().split('T')[0]}
+		onchange={setDeadlineForTask}
+	/>
+{/if}
