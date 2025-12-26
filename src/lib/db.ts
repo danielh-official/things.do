581949aabb855a1e7e9842f10a4550d3c5f8bf4f
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-26 15:58:37 -0500

    Refactor db.ts: update Item interface for clarity and consistency in parent item handling

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -17,26 +17,31 @@ export type LogStatus = null | 'completed' | 'canceled';
 // Define your entity interface
 export interface Item {
 	id: number;
+	parent_id: number | null; // The ID of the parent item in our DB (not Things ID). This should only be valid if the parent is a project or area. Furthermore, a project can only have a parent area. And an area cannot have a parent.
+	parent_things_id: string | null; // The Things ID of the parent project or area.
 	things_id: string | null; // The unique identifier from Things app.
+	type: 'task' | 'project' | 'area';
 	title: string;
-	notes: string;
-	start_date: SvelteDate | null; // If start date is today, shows in today. If start date is upcoming, shows in upcoming. If no start date, shows in anytime.
-	evening: boolean; // If true, the item would show under "This Evening" section in Today view.
-	// TODO: A possible addition to Things functionality could be to let it show for This Evening in other start dates as well.
+	notes: string | null;
+	// If it's null without a start date, it's inbox. If it has a start date, that takes precedence.
+	start: null | 'anytime' | 'someday';
+	start_date: SvelteDate | null;
 	deadline: SvelteDate | null;
-	start: 'inbox' | 'someday' | null; // If start has value, it overrides start_date rules. Someday tasks go in someday, and inbox tasks go in inbox. Null means it follows start_date rules.
-	tags: number[]; // Array of tag IDs associated with the task.
-	checklist: { title: string; logged: boolean; logged_status: LogStatus }[]; // Checklist items within the task.
-	order: number; // For ordering tasks within a list.
+	checklist: { title: string; logged: boolean; logged_status: LogStatus }[];
+	order: number;
+	tags: number[];
+	evening: boolean;
 
 	created_at: SvelteDate;
 	updated_at: SvelteDate;
+	deleted_at: SvelteDate | null;
 
-	// Open tasks are not in logbook. Completed and canceled tasks are in logbook. Any task in logbook overrides rules for start_date and start. Logbook tasks are grouped by logged date.
 	logged_at: SvelteDate | null;
-	logged_status: LogStatus; // If logged_at is not null, this field indicates whether the task was completed or canceled.
+	logged_status: LogStatus;
 
 	blocked_by: Array<number>; // Array of item IDs that block this task. This functionality is not native to Things 3.
+	later: boolean; // Indicates if the item is postponed to a later time. Not native to Things 3.
+
 }
 
 const db = new Dexie('ThingsDoDB') as Dexie & {
@@ -46,7 +51,7 @@ const db = new Dexie('ThingsDoDB') as Dexie & {
 
 // Schema declaration:
 db.version(1).stores({
-	items: '++id, things_id, order',
+	items: '++id, things_id, order, parent_id',
 	tags: '++id, parent_tag_id, order'
 });
 
