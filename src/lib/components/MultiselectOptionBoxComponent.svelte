5bfe30c096f741ca81313968c324b4e0ac1b1040
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-27 15:09:40 -0500

    Enhance MultiselectOptionBoxComponent: add restore functionality and improve button layout

--- src/lib/components/MultiselectOptionBoxComponent.svelte
+++ src/lib/components/MultiselectOptionBoxComponent.svelte
@@ -2,10 +2,12 @@
 	let {
 		highlightedItems = $bindable(),
 		deleteHighlightedItems,
+		restoreHighlightedItems,
 		clearHighlightsForAllItems
 	}: {
 		highlightedItems: Set<number>;
 		deleteHighlightedItems: () => Promise<void>;
+		restoreHighlightedItems?: () => Promise<void>;
 		clearHighlightsForAllItems: () => void;
 	} = $props();
 </script>
@@ -17,12 +19,20 @@
 			class="fixed bottom-4 left-1/2 flex -translate-x-1/2 transform flex-col space-x-4 gap-y-4 rounded border border-gray-300 bg-white p-4 shadow-lg"
 		>
 			<p>{highlightedItems.size} item(s) selected</p>
-			<div class="flex space-x-4">
+			<div class="flex flex-col space-y-4">
+				{#if !!restoreHighlightedItems}
+					<button
+						class="cursor-pointer rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+						onclick={restoreHighlightedItems}
+					>
+						Restore Selected Items
+					</button>
+				{/if}
 				<button
 					class="cursor-pointer rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600"
 					onclick={deleteHighlightedItems}
 				>
-					Delete Selected Items
+					{!!restoreHighlightedItems ? 'Permanently' : ''} Delete Selected Items
 				</button>
 				<button
 					class="cursor-pointer rounded bg-gray-500 px-4 py-2 text-white hover:bg-gray-600"
