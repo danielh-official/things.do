58bfdfc9333375157f2d5a330e460e691b86aa21
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-29 21:33:47 -0500

    Replace SVG icons with Flowbite Svelte icons in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import { resolve } from '$app/paths';
 	import { getBlockedItems, getFocusingItems, getLaterItems, getTags, getTrashedItems } from '$lib';
 	import { liveQuery } from 'dexie';
+	import { ClockOutline, CloseCircleSolid, EyeOutline, TagOutline, TrashBinOutline } from 'flowbite-svelte-icons';
 
 	let { children } = $props();
 
@@ -39,22 +40,7 @@
 							'bg-gray-300 dark:bg-gray-600': page.url.pathname === '/'
 						}}
 					>
-						<svg
-							class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							fill="none"
-							viewBox="0 0 24 24"
-							><path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M4 13h3.439a.991.991 0 0 1 .908.6 3.978 3.978 0 0 0 7.306 0 .99.99 0 0 1 .908-.6H20M4 13v6a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-6M4 13l2-9h12l2 9M9 7h6m-7 3h8"
-							/></svg
-						>
+						<EyeOutline class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75" />
 						<span class="ms-3 flex-1 whitespace-nowrap">Focusing</span>
 						{#if focusingItemsCount > 0}
 							<span
@@ -73,22 +59,7 @@
 								'bg-gray-300 dark:bg-gray-600': page.url.pathname === '/later'
 							}}
 						>
-							<svg
-								class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								fill="none"
-								viewBox="0 0 24 24"
-								><path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20ZM12 6v6l4 2"
-								/></svg
-							>
+							<ClockOutline class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75" />
 							<span class="ms-3 flex-1 whitespace-nowrap">Later</span>
 						</a>
 					</li>
@@ -102,22 +73,7 @@
 								'bg-gray-300 dark:bg-gray-600': page.url.pathname === '/blocked'
 							}}
 						>
-							<svg
-								class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								fill="none"
-								viewBox="0 0 24 24"
-								><path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20ZM12 8v4M12 16h.01"
-								/></svg
-							>
+							<CloseCircleSolid class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75" />
 							<span class="ms-3 flex-1 whitespace-nowrap">Blocked</span>
 						</a>
 					</li>
@@ -131,22 +87,7 @@
 								'bg-gray-300 dark:bg-gray-600': page.url.pathname === '/trash'
 							}}
 						>
-							<svg
-								class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								fill="none"
-								viewBox="0 0 24 24"
-								><path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M3 6h18M8 6v-1a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1m3 0v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6h15Z"
-								/></svg
-							>
+							<TrashBinOutline class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75" />
 							<span class="ms-3 flex-1 whitespace-nowrap">Trash</span>
 						</a>
 					</li>
@@ -161,22 +102,7 @@
 								'bg-gray-300 dark:bg-gray-600': page.url.pathname === '/tags'
 							}}
 						>
-							<svg
-								class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								fill="none"
-								viewBox="0 0 24 24"
-								><path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M5.5 12.5 12 19l6.5-6.5a3 3 0 0 0-4.243-4.243L12 10.914l-2.257-2.257a3 3 0 0 0-4.243 4.243Z"
-								/></svg
-							>
+							<TagOutline class="group-hover:text-fg-brand h-5 w-5 shrink-0 transition duration-75" />
 							<span class="ms-3 flex-1 whitespace-nowrap">Tags</span>
 						</a>
 					</li>
