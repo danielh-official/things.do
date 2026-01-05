b460df01123cc4d8a5c2765151e44bc4b574c18c
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-01-05 15:18:28 -0500

    Add projects page with project list and context menu functionality

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { getProjects } from '$lib';
+	import { liveQuery } from 'dexie';
+	import List from '$lib/components/List.Project.component.svelte';
+	import ContextMenu from '$lib/components/ContextMenu.svelte';
+	import DeleteSelected from '$lib/components/Buttons/DeleteSelected.button.component.svelte';
+	import ClearSelected from '$lib/components/Buttons/ClearSelected.button.component.svelte';
+	import SendToThings3 from '$lib/components/Buttons/SendToThings3.button.component.svelte';
+	import UnattachFromThings3 from '$lib/components/Buttons/UnattachFromThings3.button.component.svelte';
+
+	let projects = liveQuery(() => getProjects());
+</script>
+
+<svelte:head>
+	<title>Projects | Things.do</title>
+</svelte:head>
+
+<List {projects}>
+	{#snippet contextMenu(highlightedItems, clearHighlightsForAllItems, showMenu, menuX, menuY)}
+		<ContextMenu show={showMenu} x={menuX} y={menuY}>
+			{#snippet children()}
+				<DeleteSelected {highlightedItems} {clearHighlightsForAllItems} />
+
+				<ClearSelected {clearHighlightsForAllItems} />
+
+				<SendToThings3 {highlightedItems} />
+
+				<UnattachFromThings3 {highlightedItems} />
+			{/snippet}
+		</ContextMenu>
+	{/snippet}
+</List>
