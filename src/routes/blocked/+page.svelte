d4412a560fe909af5b201171a75318c87e04db0c
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-01-05 15:18:36 -0500

    Add blocked todos page with live query functionality

---
+++ src/routes/blocked/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { db } from '$lib/db';
+	import { getBlockedTodos } from '$lib';
+	import { liveQuery } from 'dexie';
+
+	let blockedTodos = liveQuery(() => getBlockedTodos());
+</script>
+
+<svelte:head>
+	<title>Blocked | Things.do</title>
+</svelte:head>
