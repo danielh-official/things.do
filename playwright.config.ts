ba9e879c80835e1a9656556042d58b198f03c066
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-01-06 15:57:48 -0500

    Update Playwright configuration to include timeout setting for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: { command: 'npm run build && npm run preview', port: 4173 },
-	testDir: 'e2e'
+	testDir: 'e2e',
+	timeout: 10000 // 10 seconds per test
 });
