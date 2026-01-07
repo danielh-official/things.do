330b266df3b1312e7f8cab81c351ef52afb6419e
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-01-07 16:45:47 -0500

    Remove unused import of devices from Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: { command: 'npm run build && npm run preview', port: 4173 },
